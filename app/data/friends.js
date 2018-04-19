cf53df822b910428b113691ab9fe3c11f430be2e
Michael Johnson <michaeljohnson@Michaels-MacBook-Pro.local>
2018-04-19 14:21:40 -0700

    fixed modal display time

--- app/data/friends.js
+++ app/data/friends.js
@@ -3,6 +3,21 @@ var userProfiles = [
         name: "Michael",
         profPic: "https://scontent.fsan1-1.fna.fbcdn.net/v/t1.0-9/23658374_1508300452558109_2647867184239099904_n.jpg?_nc_cat=0&oh=8adb5415900bc2a26eac232160c9c3c1&oe=5B638F3F",
         answers: [2,2,2,2,2,2,2,2,2,2]
+    },
+    {
+        name: "Daenerys",
+        profPic: "https://media.vanityfair.com/photos/59a589d562a7f43bf00ac4cd/master/pass/disgust-incest-daenerys.jpg",
+        answers: [2,4,3,4,2,1,5,4,3,2]
+    },
+    {
+        name: "Tyrion",
+        profPic: "http://hdqwalls.com/wallpapers/tyrion-lannister-game-of-thrones-new.jpg",
+        answers: [1,2,3,4,5,1,2,3,4,5]
+    },
+    {
+        name: "Arya",
+        profPic: "http://vignette1.wikia.nocookie.net/gameofthrones/images/e/e9/Arya_Stark_4.jpg/revision/latest/scale-to-width-down/2000?cb=20140428152515",
+        answers: [5,4,3,2,1,5,4,3,2,1]
     }
 ];
 
