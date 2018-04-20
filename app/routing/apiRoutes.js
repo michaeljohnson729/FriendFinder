4f1ff22b107c860f28b170bf419f37446b112182
Michael Johnson <michaeljohnson@Michaels-MacBook-Pro.local>
2018-04-19 20:08:28 -0700

    display random images, couldn't figure out algorhythem

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,13 +1,15 @@
 var userProfiles = require("../data/friends");
-module.exports = function(app){
+module.exports = function (app) {
 
-    app.get("/api/friends", function (req, res){
-        return res.json(userProfiles);
-    });    
-    app.post("/api/friends", function(req, res){
+    app.get("/api/friends", function (req, res) {
+        console.log(req.body);
+            return res.json(userProfiles);
+    
+    });
+    app.post("/api/friends", function (req, res) {
         var newUser = req.body;
         console.log(newUser);
         userProfiles.push(newUser);
         res.end();
     });
-}
+};
